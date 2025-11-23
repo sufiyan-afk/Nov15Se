975abac05bbcb7face791c4c40bd9c69fead346c
sufiyan-afk <widow5147@gmail.com>
2025-11-23 22:48:25 +0530

    Add initial C source file my into.c.C

---
+++ my into.c.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf ("*********************************************************************\n");
+	printf ("*          NAME      : SUFIYAN ASIF TAL                             *\n");
+	printf ("*          PROGRAM   : MY BIO-DATA                                  *\n");
+	printf ("*          DATE      : 22-NOV-2025                                  *\n");
+	printf ("*          COURSE    : TOPS TECHNOLOGIES SURAT- PYTHON FULL STACK   *\n");
+	printf ("*                                                                   *\n");
+	printf ("*********************************************************************\n");
+	
+	return 0;
+		}
+
